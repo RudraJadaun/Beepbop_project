df5f59873b6c40b5b4b3e782193257001eba5fa6
Yashashvi Jadaun <Yashashvi1004@gmail.com>
2023-11-14 23:15:57 +0530

    New project push

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Jobs from './pages/Jobs';
+import Profile from './pages/Profile';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="home" element={<Home />} />
+        <Route path="jobs" element={<Jobs />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
